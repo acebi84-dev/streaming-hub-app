b0c27c62d6804a295bf4393bc85c963ec58830e7
Alkın <acebi84@gmail.com>
2026-05-22 19:09:29 +0300

    fix src/app structure

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
-
 const config = getDefaultConfig(__dirname);
-
+config.transformer = {
+  ...config.transformer,
+  unstable_allowRequireContext: true,
+};
+config.resolver = {
+  ...config.resolver,
+  unstable_enableSymlinks: false,
+};
 module.exports = config;
