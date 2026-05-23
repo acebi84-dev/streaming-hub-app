b8773246ad5ec99e5471592492d965acdf9e20cd
Alkın <acebi84@gmail.com>
2026-05-23 11:25:23 +0300

    mock opentelemetry for hermes

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,25 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
 const config = getDefaultConfig(__dirname);
+
 config.transformer = {
   ...config.transformer,
   unstable_allowRequireContext: true,
 };
+
 config.resolver = {
   ...config.resolver,
   unstable_enableSymlinks: false,
-  blockList: [
-    /node_modules\/@opentelemetry\/.*/,
-  ],
+  resolveRequest: (context, moduleName, platform) => {
+    if (moduleName.startsWith('@opentelemetry/')) {
+      return {
+        filePath: path.resolve(__dirname, 'mocks/opentelemetry.js'),
+        type: 'sourceFile',
+      };
+    }
+    return context.resolveRequest(context, moduleName, platform);
+  },
 };
+
 module.exports = config;
